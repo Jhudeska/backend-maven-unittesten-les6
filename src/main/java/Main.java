a65537fd261d348a3712049cea88416caca5edd7
Jhudeska Ignacia <jvsignacia@live.nl>
2026-06-27 12:25:32 +0200

    StringOps class afgemaakt en unit test. Beide klassen StrOps en Person in main getest

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
 class Main {
     public static void main(String[] args) {
+        System.out.println("This is is an project for unittesten and maven");
+
+        StringOps strOps = new StringOps();
+        System.out.println(strOps.reverse("hello"));
+
+        Person person = new Person();
+        System.out.println(person.getName());
 
     }
 }
